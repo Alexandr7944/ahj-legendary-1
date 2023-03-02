a893dd39912ee2359ce149e3746ab0c61bf9288d
Alexander Vasnev <a-s-vasnev88@yandex.ru>
2023-03-02 16:42:04 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 import Goblin from "./Goblin";
-import Win from "./Win";
 
 const goblin = new Goblin(document.querySelector(".game"));
-// const win = new Win(document.querySelector(".game"));
+goblin.startGame();
